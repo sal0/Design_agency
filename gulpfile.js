b6cd50bbab980b16ac0563f0a7391baad0da5097
sal0 <salomemindiashvili5@gmail.com>
2019-07-19 01:13:45 +0400

    added section #2

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+
 sass.compiler = require('node-sass');
- 
+
 gulp.task('sass', function () {
-  return gulp.src('./scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('styles'));
+    return gulp.src('./scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('styles'));
 });
- 
+
 gulp.task('watch', function () {
     gulp.watch('scss/*.scss', gulp.parallel('sass'));
-  });
+});
 
